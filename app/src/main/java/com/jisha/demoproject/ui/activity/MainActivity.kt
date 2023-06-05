f4f4179580fa6c868900eef11f5e3e8db2296117
jishameridian <jisha@meridian.net.in>
2023-06-05 12:14:12 +0530

    main activity changed

--- app/src/main/java/com/jisha/demoproject/ui/activity/MainActivity.kt
+++ app/src/main/java/com/jisha/demoproject/ui/activity/MainActivity.kt
@@ -1,15 +1,54 @@
 package com.jisha.demoproject.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.jisha.demoproject.R
-import com.onesignal.OneSignal
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.jisha.demoproject.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding?.maps?.onCreate(savedInstanceState)
+        setContentView(binding?.root)
+        init()
 
+    }
+
+    private fun init() {
+        binding?.maps?.getMapAsync(this)
+    }
+
+     override fun onMapReady(googleMap: GoogleMap) {
+         val location = LatLng(11.2485, 75.8336)
+         binding?.maps?.getMapAsync {gmp->
+             gmp.clear()
+             gmp.addMarker(MarkerOptions().position(location).title("Marker in San Francisco"))
+             gmp.animateCamera(CameraUpdateFactory.newLatLngZoom(location, 18f))
+    }
+     }
+    override fun onResume() {
+        binding?.maps?.onResume()
+        super.onResume()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding?.maps?.onDestroy()
+    }
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding?.maps?.onLowMemory()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding?.maps?.onPause()
 
     }
 }
